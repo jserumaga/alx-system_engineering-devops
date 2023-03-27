2caec99271c681156b0d95ae2048a723b51238f6
jserumaga <j.serumaga@gmail.com>
2023-03-27 10:34:40 -0700

    Files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Modifying the ssh_config file, to configure the private key
+# in ~ /.ssh /school and for password authentication.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
---
+++ 0x0B-ssh/README.md
@@ -0,0 +1,6 @@
+This is a README.md file for 0x0B. SSH project
+This project has the following task;
+0. Use a private key
+1. Create an SSH key pair
+2. Client configuration file
+3. Let me in!
